02be169ec5b589d3cd3830fd5cd6e560889600bd
dev-om-saran <omsaran48@gmail.com>
2023-07-07 15:52:34 +0530

    second commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var projectRouter = require('./routes/project');
 
-var session = require('express-session');
+var session = require('cookie-session');
 var flash = require('connect-flash');
 
 var app = express();
